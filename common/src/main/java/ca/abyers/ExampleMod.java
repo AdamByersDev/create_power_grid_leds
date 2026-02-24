bd5859460f592bcd6cedccf1d4f8a0073cb28be3
Adam Byers <adam@abyers.ca>
2026-02-24 05:31:02 -0500

    First commit

---
+++ common/src/main/java/ca/abyers/ExampleMod.java
@@ -0,0 +1,9 @@
+package ca.abyers;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "create_power_grid_leds";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
