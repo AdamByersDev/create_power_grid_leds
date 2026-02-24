db5c364e5f5c9214cfd73f0b86576e1a9f6da440
Adam Byers <adam@abyers.ca>
2026-02-24 13:59:41 -0500

    Added creative tab and recipies

--- common/src/main/java/ca/abyers/ExampleMod.java
+++ common/src/main/java/ca/abyers/ExampleMod.java
@@ -1,11 +1,13 @@
 package ca.abyers;
 
 import ca.abyers.registry.ModItems;
+import ca.abyers.registry.ModCreativeTabs;
 
 public final class ExampleMod {
     public static final String MOD_ID = "create_power_grid_leds";
 
     public static void init() {
         ModItems.register();
+        ModCreativeTabs.register();
     }
 }
---
+++ common/src/main/java/ca/abyers/registry/ModCreativeTabs.java
@@ -0,0 +1,32 @@
+package ca.abyers.registry;
+
+import ca.abyers.ExampleMod;
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public final class ModCreativeTabs {
+    private static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(ExampleMod.MOD_ID, Registries.CREATIVE_MODE_TAB);
+
+    public static final RegistrySupplier<CreativeModeTab> MAIN = CREATIVE_MODE_TABS.register(
+            "main",
+            () -> CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
+                    .title(Component.translatable("itemGroup." + ExampleMod.MOD_ID))
+                    .icon(() -> new ItemStack(ModItems.LED_BULB.get()))
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItems.LED_BULB.get());
+                        output.accept(ModItems.LV_LED_BULB.get());
+                    })
+                    .build());
+
+    private ModCreativeTabs() {
+    }
+
+    public static void register() {
+        CREATIVE_MODE_TABS.register();
+    }
+}
