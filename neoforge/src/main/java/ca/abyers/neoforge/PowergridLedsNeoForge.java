74de3bce29deba17e20705be4189d977d3069b9c
Cosmo on FusionVoyager3 <cosmo@pika-os.com>
2026-08-11 23:05:23 +0300

    Port to 1.21.1 Neoforge

--- neoforge/src/main/java/ca/abyers/neoforge/PowergridLedsNeoForge.java
+++ neoforge/src/main/java/ca/abyers/neoforge/PowergridLedsNeoForge.java
@@ -1,14 +1,14 @@
-package ca.abyers.forge;
+package ca.abyers.neoforge;
 
-import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import dev.architectury.platform.neoforge.EventBuses;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import ca.abyers.PowergridLeds;
 
 @Mod(PowergridLeds.MOD_ID)
-public final class PowergridLedsForge {
-    public PowergridLedsForge() {
+public final class PowergridLedsNeoForge {
+    public PowergridLedsNeoForge() {
         // Submit our event bus to let Architectury API register our content on the right time.
         EventBuses.registerModEventBus(PowergridLeds.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 
